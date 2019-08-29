961f5a4672cf7661eb48e1f65eb442772f40305c
AshTNA <ashleyaburnett@gmail.com>
2019-08-29 10:34:07 +0100

    Make innerText dynamic

--- tna-glossary-expander.js
+++ tna-glossary-expander.js
@@ -1,3 +1,12 @@
+/* Takes the title of a glossary term, and splits it by the first instance of 'charge',
+and then creates more meaningful added text for the glossary term.
+
+The splitting is done in order to make the innerText be dynamic (e.g. if one research guide says
+'There may be a charge' and one says 'There will be a charge' it can work for both of those glossary terms.
+
+*/
+
+
 let glossary_terms = document.querySelectorAll('.research-guide-glossary-term');
 
 glossary_terms.forEach((term) => {
@@ -6,11 +15,9 @@ glossary_terms.forEach((term) => {
 
         term.classList.add('glossary-expand');
 
-        // let first_sentence =  term.title.split('.')[0];
+        let first_sentence =  term.title.split('charge')[0];
 
-        let first_sentence =  'There will be a charge';
-
-        term.innerText = first_sentence;
+        term.innerText = first_sentence + ' charge.';
 
         term.addEventListener('click',  (e) => {
             window.alert(term.title);
