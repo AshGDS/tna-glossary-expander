92aff1bde335cd6a82fc1a83aabc5a8951746123
AshTNA <ashleyaburnett@gmail.com>
2019-11-15 14:09:30 +0000

    Add aria-labels and minor formatting

--- tna-glossary-expander.js
+++ tna-glossary-expander.js
@@ -1,15 +1,16 @@
 /*
 * This script firstly converts plain text (£) in research guides into glossary terms.
 * The script will then loop through every instance of the glossary terms, and make them accessible.
-* */
+* It does it in this order as some (£) are already glossary terms and some are not, so it ensures all are the same
+* before making them accessible.
+*/
 
 
 $(function() {
 
     let make_plain_text_into_glossary_terms = function() {
-        let research_guide_text = $('#page_wrap .row:nth-of-type(2) p');
 
-        console.log('Paragraphs:', research_guide_text.length);
+        let research_guide_text = $('#page_wrap .row:nth-of-type(2) p');
 
         $(research_guide_text).each(function () {
 
@@ -25,13 +26,11 @@ $(function() {
                 $(this).html(p_html);
 
             }
-
-
         });
-
     };
 
    let make_glossary_terms_accessible = function () {
+
        let research_guide_glossary_terms = $('.research-guide-glossary-term');
 
        if($(research_guide_glossary_terms).length > 0) {
@@ -41,23 +40,18 @@ $(function() {
                if($(this).text() === '£') {
 
                    $(this).addClass('glossary-expand');
-
                    $(this).text('charges apply');
+                   $(this).attr('aria-label', $(this).attr('title'));
 
                    $(this).click(function() {
 
                        alert(this.title);
                    });
                }
-
            });
-
        }
    };
 
    make_plain_text_into_glossary_terms();
    make_glossary_terms_accessible();
-
-
-
 });
